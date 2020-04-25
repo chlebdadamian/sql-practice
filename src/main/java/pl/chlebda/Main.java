176fc739af41caf49934b92079194111ea0ff752
Damian Chlebda <damien.chlebda@gmail.com>
2020-04-25 21:30:49 +0200

    add more query

--- src/main/java/pl/chlebda/Main.java
+++ src/main/java/pl/chlebda/Main.java
@@ -2,34 +2,51 @@ package pl.chlebda;
 
 import pl.chlebda.model.Artist;
 import pl.chlebda.model.Datasource;
+import pl.chlebda.model.SongArtist;
 
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-
         Datasource datasource = new Datasource();
         if (!datasource.open()) {
-            System.out.println("Cannot open datasource");
+            System.out.println("Can't open datasource");
             return;
         }
 
-        List<Artist> artists = datasource.queryArtists();
-        if (artists == null || artists.isEmpty()) {
-
-            System.out.println("Cannot open datasource");
+        List<Artist> artists = datasource.queryArtists(5);
+        if (artists == null) {
+            System.out.println("No artists!");
             return;
-
         }
 
         for (Artist artist : artists) {
-
             System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
-
         }
-        datasource.close();
 
+        List<String> albumsForArtist =
+                datasource.queryAlbumsForArtist("Carole King", Datasource.ORDER_BY_ASC);
+
+        for (String album : albumsForArtist) {
+            System.out.println(album);
+        }
+
+        List<SongArtist> songArtists = datasource.queryArtistsForSong("Go Your Own Way", Datasource.ORDER_BY_ASC);
+        if (songArtists == null) {
+            System.out.println("Couldn't find the artist for the song");
+            return;
+        }
+
+        for (SongArtist artist : songArtists) {
+            System.out.println("Artist name = " + artist.getArtistName() +
+                    " Album name = " + artist.getAlbumName() +
+                    " Track = " + artist.getTrack());
+        }
+
+        datasource.querySongsMetadata();
+
+        datasource.close();
     }
 
 }
